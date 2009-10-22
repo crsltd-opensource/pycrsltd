d39a6b2df59016f3cd4677a43c3d9c0d3c352b53
Valentin Haenel <valentin.haenel@gmx.de>
2009-10-22 20:41:35 +0200

    rewrite most of existing code, can now read eeprom

--- pyoptical.py
+++ pyoptical.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 
 import serial
 
@@ -6,11 +5,18 @@ ACK='\x06'
 NACK='\x15'
 
 class OptiCal(object):
-    def __init__(com_port,debug=True, timeout=10):
-        self.ser = serial.Serial(com_port,timeout=timeout)
+    def __init__(self, com_port,debug=True, timeout=10):
+        self.ser = serial.Serial(com_port, timeout=timeout)
+
     def _calibrate(self):
         self.ser.write('C')
-        ret = self.read
-        if ret != ACK:
-            raise RuntimException('Optical returned NACK byte')
-    
+        ret = self.ser.read()
+        if ret == "":
+            print "should raise excpetion since reading timed out"
+        if ret == NACK:
+            print "should raise excpetion due to NACK"
+
+    def _read_eeprom(self, address):
+        self.ser.write(chr(128+address))
+        return self.ser.read(2)[0]
+

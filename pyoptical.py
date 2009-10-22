f480059bcf8e3d668c1760064092b2110a2a5284
Valentin Haenel <valentin.haenel@gmx.de>
2009-10-22 19:51:39 +0200

    adding base class and calibration routine

---
+++ pyoptical.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import serial
+
+ACK='\x06'
+NACK='\x12'
+
+class OptiCal(object):
+    def __init__(com_port,debug=True, timeout=10):
+        self.ser = serial.Serial(com_port,timeout=timeout)
+    def _calibrate(self):
+        self.ser.write('C')
+        ret = self.read
+        if ret != ACK:
+            raise RuntimException('Optical returned NACK byte')

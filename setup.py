80ee949df1a8249c4e51bd4f39ac38e7719a0557
Valentin Haenel <valentin.haenel@gmx.de>
2009-11-05 14:51:27 +0100

    adding setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyoptical',
+      version='0.1',
+      description='Pure python interface to OptiCAL',
+      author='Valentin Haenel',
+      author_email='valentin.haenel@gmx.de',
+      py_modules=['pyoptical'],
+     )
+

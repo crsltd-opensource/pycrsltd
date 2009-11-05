0bcaffd4199fff139d9a947bb7d4723b0ce2a56b
Valentin Haenel <valentin.haenel@gmx.de>
2009-11-05 15:10:00 +0100

    add license

--- pyoptical.py
+++ pyoptical.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python
 #coding=utf-8
 
+# Copyright (c) 2009 Valentin Haenel
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 """ pyoptical - a pure python interface to the CRS 'OptiCal' photometer
 
     @author:  Valentin Haenel <valentin.haenel@gmx.de>
@@ -93,6 +113,10 @@ class OptiCal(object):
                 a) To read out the ADC value, an 'L' must be sent instead of an 'R'
                 b) The equations to convert from ADC to meaningful units had changed. See
                 read_luminance() and read_voltage() for details.
+
+            The corrected versions of the conversion formulas can found in rge
+            OptiCAL.py python interface available from the CRS website, written
+            by Walter F. Bischof in 2007.
 
             The constructor will first perform the initial calibration of the
             device as required by the protocol specification. Next it will read

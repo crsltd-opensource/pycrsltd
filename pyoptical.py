1171d20cd113e1a10f6f25d43a2566cedce92486
Valentin Haenel <valentin.haenel@gmx.de>
2009-12-08 16:11:49 +0100

    version bump

--- pyoptical.py
+++ pyoptical.py
@@ -24,7 +24,7 @@
 """ pyoptical - a pure python interface to the CRS 'OptiCAL' photometer
 
     @author:  Valentin Haenel <valentin.haenel@gmx.de>
-    @version: 0.1
+    @version: 0.2-dev
 
     This module provides the 'OptiCAL' class and some supporting code.
 

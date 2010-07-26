fe531ff0ce7d9e22e581301bb1936bb6bee259ea
Valentin Haenel <valentin.haenel@gmx.de>
2010-07-26 15:42:31 +0200

    version bump 0.3-dev -> 0.3

--- pyoptical.py
+++ pyoptical.py
@@ -166,7 +166,7 @@
     be put into 'current' mode at startup.
 
 """
-__version__ = "0.3-dev"
+__version__ = "0.3"
 __author__ = "Valentin Haenel <valentin.haenel@gmx.de>"
 __docformat__ = "restructuredtext en"
 

e81088130c5bd3a9741a3fac859ec86da0960631
Jon Peirce <jon@peirce.org.uk>
2011-02-09 12:20:26 +0000

    created repository

---
+++ README.txt
@@ -0,0 +1,8 @@
+This is the central library for the python interface to CRS Ltd hardware (Bits++,
+ColorCal etc...).
+
+The library is very much under construction and should not be used yet except by
+enthusiasts. 
+
+It is being written mostly by Jon Peirce, with support from CRS Ltd. If you would
+like to contribute please get in touch!
---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+"""Requires setuptools.
+
+To build the setuptools egg use
+    python setup.py bdist_egg
+and either upload it to the PyPI with:
+    python setup.py upload
+or upload to your own server and register the release with PyPI:
+    python setup.py register
+
+A source distribution (.zip) can be built with
+    python setup.py sdist --format=zip
+That uses the manifest.in file for data files rather than searching for them here
+"""
+
+from setuptools import setup, Extension, find_packages
+import sys
+sys.path.append('src')#so we can import openpyxl
+import openpyxl#to fetch __version__ etc
+
+setup(name = "pycrsltd",
+    packages = find_packages('pycrsltd'),
+    include_package_data = True,
+    package_data = {}, #DOESN'T AFFECT zip DISTRIBUTION. MUST MODIFY MANIFEST.in TOO
+    #metadata
+    version = pycrsltd.__version__,
+    description = "A Python library to interface with CRS Ltd hardware",
+    long_description = "A Python library to interface with CRS Ltd hardware",
+    author = pycrsltd.__author__,
+    author_email = pycrsltd.__author_email__,
+    url = pycrsltd.__url__,
+    license = pycrsltd.__license__,
+    download_url = pycrsltd.__downloadUrl__,
+    test_suite = 'nose.collector',
+    classifiers = ['Development Status :: 4 - Beta',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python'],
+    )
